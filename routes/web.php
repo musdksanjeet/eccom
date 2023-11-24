c9f7faa4e871c50747683e35d300fb477a4bea53
musdksanjeet <sanjeetsingh7877@gmail.com>
2023-11-24 15:37:41 +0530

    review

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ use App\Http\Livewire\Admin\AdminOrderComponent;
 use App\Http\Livewire\Admin\AdminOrderDetailsComponent;
 use App\Http\Livewire\User\UserOrdersComponent;
 use App\Http\Livewire\User\UserOrderDetailsComponent;
+use App\Http\Livewire\User\UserReviewComponent;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -94,5 +95,5 @@ Route::middleware(['auth:sanctum','verified'])->group(function(){
     Route::get('/user/dashboard',UserDashboardComponent::class)->name('user.dashboard');
     Route::get('/user/orders',UserOrdersComponent::class)->name('user.orders');
     Route::get('/user/orders/{order_id}',UserOrderDetailsComponent::class)->name('user.orderdetails');
-  
+    Route::get('/user/review/{order_item_id}',UserReviewComponent::class)->name('user.review');
 });
