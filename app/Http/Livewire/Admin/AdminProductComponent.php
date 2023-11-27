2adbd750657464849ef4fc4d4d16fcd7ce12416d
musdksanjeet <sanjeetsingh7877@gmail.com>
2023-11-27 16:23:18 +0530

    addproduct

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -11,6 +11,21 @@ class AdminProductComponent extends Component
 
     public function deleteProduct($id){
         $product = Product::find($id);
+        if($product->image)
+        {
+            unlink('assets/images/products'.'/'.$product->image);
+        }   
+        if($product->images)
+        {
+            $images = explode(",",$product->images);
+            foreach($images as $image)
+            { 
+                if($image)
+                {
+                    unlink('assets/images/products'.'/'.$image);  
+                }                                       
+            }
+        } 
         $product->delete();
         session()->flash('message','Product has been deleted successfully!');
 
