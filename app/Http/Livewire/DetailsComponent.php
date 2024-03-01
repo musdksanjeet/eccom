bba1e30fb5b80494529d8589c41dc9508f455c50
musdksanjeet <127778300+musdksanjeet@users.noreply.github.com>
2024-03-01 11:31:35 +0530

    Update DetailsComponent.php
    add

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -27,7 +27,7 @@ class DetailsComponent extends Component
     }
 
     public function store($product_id,$product_name,$product_price){
-        Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        Cart::add($product_id,$product_name,$this->qty,$product_price)->associate('App\Models\Product');
         session()->flash('success_message','Item has been added successfully!');
         return redirect()->route('product.cart');
     }
